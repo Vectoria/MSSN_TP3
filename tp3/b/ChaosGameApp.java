546b2c937571774f9c59530b84faa20bd4c09fcc
deSoos <fsousa2004@gmail.com>
2023-12-22 15:34:08 +0000

    user interaction to make different shapes

--- tp3/b/ChaosGameApp.java
+++ tp3/b/ChaosGameApp.java
@@ -4,13 +4,22 @@ import processing.core.PApplet;
 import processing.core.PVector;
 import tp3.processing.IProcessingApp;
 
+import java.util.Scanner;
+
 public class ChaosGameApp implements IProcessingApp {
     private ChaosGame cg;
     @Override
     public void setup(PApplet p) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please, insert the number of vertices you want you shape to have.");
+        int chosenVerticeNum = keyboard.nextInt();
+        while (chosenVerticeNum < 3) {
+            System.out.println("You can't have a shape with less than 3 vertices, re-insert your value");
+            chosenVerticeNum = keyboard.nextInt();
+        }
         cg = new ChaosGame();
-        cg.initShape(p);
         cg.drawInitialRandomPoint(p);
+        cg.initShape(p, chosenVerticeNum);
     }
     @Override
     public void draw(PApplet p, float dt) {
